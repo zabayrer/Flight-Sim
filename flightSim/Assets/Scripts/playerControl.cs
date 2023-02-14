01114c58eccbf08dbbf811ac10d8a537f34d11a2
zabayrer <122053629+zabayrer@users.noreply.github.com>
2023-02-14 10:57:55 -0800

    Deleted spawn manager to start again. Also froze player on collision with the world.

--- flightSim/Assets/Scripts/playerControl.cs
+++ flightSim/Assets/Scripts/playerControl.cs
@@ -5,38 +5,48 @@ using UnityEngine;
 public class playerControl : MonoBehaviour
 {
     //private variables
+    private Rigidbody playerRb;
     private float speed = 20.0f;
     public float throttle = 0.0f;
     private float turnSpeed = 25;
     private float horizontalInput;
     private float forwardInput;
+    public bool gameOver = false;
     public GameObject projectilePrefab;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        playerRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //player input
-        horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Vertical");
+        //make sure player can't do anything after they die
+        if (gameOver == false) 
+        {
+            //player input
+            horizontalInput = Input.GetAxis("Horizontal");
+            forwardInput = Input.GetAxis("Vertical");
+            //vehicle pitch
+            transform.Rotate(Vector3.up * Time.deltaTime * speed * horizontalInput);
+            //vehicle yaw
+            transform.Rotate(Vector3.right, Time.deltaTime * turnSpeed * forwardInput);
+            //vehicle roll
+            transform.Rotate(Vector3.forward * Time.deltaTime * turnSpeed * -horizontalInput);
 
-        //vehicle pitch
-        transform.Rotate(Vector3.up * Time.deltaTime * speed * horizontalInput);
-
-        //vehicle yaw
-        transform.Rotate(Vector3.right, Time.deltaTime * turnSpeed * forwardInput);
-        //vehicle roll
-        transform.Rotate(Vector3.forward * Time.deltaTime * turnSpeed * -horizontalInput);
+            //when space is pressed, launch a projectile
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Instantiate(projectilePrefab, transform.position, transform.rotation);
+            }
+        }
 
         //vehicle throttle
         transform.Translate(Vector3.forward * Time.deltaTime * throttle);
         //change throttle
-        if (Input.GetKey("="))
+        if (Input.GetKey("=") && !gameOver)
         {
             if (throttle < 100)
                 throttle = throttle + 1;
@@ -56,12 +66,25 @@ public class playerControl : MonoBehaviour
         {
             //hard-coded values go brr
             transform.position = new Vector3(transform.position.x, -25, transform.position.z);
-            Debug.Log("game over");
+            
+            gameOver = true;
         }
-        //when space is pressed, launch a projectile
-        if (Input.GetKeyDown(KeyCode.Space))
+       
+        //when player dies
+        if (gameOver == true)
         {
-            Instantiate(projectilePrefab, transform.position, transform.rotation);
+            Debug.Log("game over");
+            throttle = 0;
         }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("WorldObject"))
+            {
+                gameOver = true;
+                
+                
+            }
     }
 }
