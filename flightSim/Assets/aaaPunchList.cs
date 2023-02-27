2d045f98a916aaa1c5091dbb99b6477acd562745
zabayrer <122053629+zabayrer@users.noreply.github.com>
2023-02-27 11:23:41 -0800

    changed things in punch list

--- flightSim/Assets/aaaPunchList.cs
+++ flightSim/Assets/aaaPunchList.cs
@@ -1,10 +1,10 @@
 /*
---make projectile have propper rotation, make projectile come out at center of plane
+--make projectile come out at center of plane
 
 
 2. make world
 3. make projectile prefab into propper projectile
-4. rotate target randomly if possible
+4. rotate target randomly if possible: using a rotating spawn manager. not working?
 7. Add runways as part of world, make landing possible
 8. figure out why wing clips through ground. Shouldn't be a problem in final version.
 9. animation (?) and sound effects
